cd42323e91ab62bf2033e6ab2d50f659a28654c4
Luv Gangwani <luv.gangwani@gmail.com>
2019-11-24 01:03:18 +1100

    Chapter 2 - Starting your project

---
+++ app/Http/Controllers/ShowRoomsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ShowRoomsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+        // return response("A listing of rooms", 200);
+
+        $rooms = DB::table('rooms')->get();
+
+        if($request->query('id') !== null) {
+            $rooms = $rooms->where('room_type_id', $request->query('id'));
+        }
+
+        // return response()->json($rooms);
+        return view('rooms.index', ['rooms' => $rooms]);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function() {
+    return "Hello World from web.php";
+});
+
+Route::get('/rooms', 'ShowRoomsController');
+
+/* Route::get('/bookings', 'BookingController@index');
+Route::get('/bookings/create', 'BookingController@create');
+Route::post('/bookings', 'BookingController@store');
+Route::get('/bookings/{booking}', 'BookingController@show'); // Route model binding 
+Route::get('/bookings/{booking}/edit', 'BookingController@edit');
+Route::put('/bookings/{booking}', 'BookingController@update');
+Route::delete('/bookings/{booking}', 'BookingController@destroy'); */
+
+// Replace the above lines by
+
+Route::resource('bookings', 'BookingController');
